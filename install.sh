b1eee9f455f677a9356495aae382d4879a6c5cc3
Jakukyo Friel <weakish@gmail.com>
2013-11-09 16:09:26 +0800

    add Manpages
    - add bats.1
    - add bats.7
    - tweak install.sh to install manpages

--- install.sh
+++ install.sh
@@ -31,5 +31,7 @@ BATS_ROOT="$(abs_dirname "$0")"
 mkdir -p "$PREFIX"/{bin,libexec}
 cp -R "$BATS_ROOT"/bin/* "$PREFIX"/bin
 cp -R "$BATS_ROOT"/libexec/* "$PREFIX"/libexec
+cp "$BATS_ROOT"/man/bats.1 "$PREFIX"/share/man/man1
+cp "$BATS_ROOT"/man/bats.7 "$PREFIX"/share/man/man7
 
 echo "Installed Bats to $PREFIX/bin/bats"
